1b19a2a19f3140cc218353ec6c700c7b3642dce5
caoweinian <2840083875@qq.com>
2021-01-17 22:48:56 +0800

    testing

---
+++ LocalOJ/src/prerequisite/target/debug/deps/prerequisite.d
@@ -0,0 +1,5 @@
+E:\JetBrains\source\others\LocalOJ\LocalOJ\src\prerequisite\target\debug\deps\prerequisite.exe: src\main.rs
+
+E:\JetBrains\source\others\LocalOJ\LocalOJ\src\prerequisite\target\debug\deps\prerequisite.d: src\main.rs
+
+src\main.rs:
---
+++ LocalOJ/src/prerequisite/target/debug/prerequisite.d
@@ -0,0 +1,1 @@
+E:\JetBrains\source\others\LocalOJ\LocalOJ\src\prerequisite\target\debug\prerequisite.exe: E:\JetBrains\source\others\LocalOJ\LocalOJ\src\prerequisite\src\main.rs
